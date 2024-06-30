0862eabf8cabd52405b3280cf7be7681729b7ac9
sukantadutta123 <141480350+sukantadutta123@users.noreply.github.com>
2024-06-30 21:49:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+sign_up_btn2.addEventListener("click", () => {
+    container.classList.add("sign-up-mode2");
+});
+
+sign_in_btn2.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode2");
+});
+
+
+
+/* Captcha */
+
+function generateCaptcha() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let captcha = '';
+    for (let i = 0; i < 6; i++) {
+        captcha += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return captcha;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const captchaDisplay = document.getElementById('captcha-display');
+    const captcha = generateCaptcha();
+    captchaDisplay.textContent = captcha;
+});
+
+/* Password Eye */
+document.querySelectorAll('.toggle-password').forEach(eyeIcon => {
+    eyeIcon.addEventListener('click', function() {
+        const passwordInput = this.previousElementSibling;
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            this.classList.remove('fa-eye');
+            this.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            this.classList.remove('fa-eye-slash');
+            this.classList.add('fa-eye');
+        }
+    });
+});
